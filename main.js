83ce2cfc2244e8e4ae5918ca726a64a55e9d1bf4
Peter Leriche <R0370657@student.thomasmore.be>
2016-10-16 21:42:03 +0200

    Code van Stackexchange gekopieerd, testen wat het effect is

--- main.js
+++ main.js
@@ -4,4 +4,42 @@
  * and open the template in the editor.
  */
 
+/**
+ * This class defines a CalculatorResource.
+ */
 
+@Path("v{version}/calculators/{id}")
+@Named
+public class CalculatorResource {
+
+    private final Long value;
+
+    public CalculatorResource() {
+        value = 0L;
+    }
+
+    public CalculatorResource(Long initialValue) {
+        this.value = initialValue;
+    }
+
+    @GET
+    public Long getValue() {
+        return value;
+    }
+
+    @Path("+{value}")
+    public CalculatorResource add(@PathParam("value") Long value) {
+        return new CalculatorResource(this.value + value);
+    }
+
+    @Path("-{value}")
+    public CalculatorResource subtract(@PathParam("value") Long value) {
+        return new CalculatorResource(this.value - value);
+    }
+
+    @Path("*{value}")
+    public CalculatorResource multiply(@PathParam("value") Long value) {
+        return new CalculatorResource(this.value * value);
+    }
+
+}
