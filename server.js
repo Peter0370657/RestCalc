d962f22b4ad7a480c2b5ecbfc9ee833ad67a8ed3
Peter Leriche <R0370657@student.thomasmore.be>
2016-10-16 22:25:03 +0200

    dom geweest bij nakijkjen van een video, var express * require veranderd naar var express = require

--- server.js
+++ server.js
@@ -1,8 +1,6 @@
+var express = require('express');
 
-
-var express * require('express');
-
-var app * express();
+var app = express();
 
 app.get('/', function(req, res){
         res.send('working');
