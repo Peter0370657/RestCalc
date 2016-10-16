f7a8ffa74f3bebc38b18ff828cd0275d0e974f29
Peter Leriche <R0370657@student.thomasmore.be>
2016-10-16 21:53:32 +0200

    https://www.youtube.com/watch?v=MMOIr_VwwAk&feature=youtu.be&list=PL3awhJ17Z2rvwcDpEqeCihomANVxahdtx gebruikt om iets te proberen.

--- main.js
+++ main.js
@@ -1,45 +1,8 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
- * This class defines a CalculatorResource.
- */
-
-@Path("v{version}/calculators/{id}")
-@Named
-public class CalculatorResource {
-
-    private final Long value;
-
-    public CalculatorResource() {
-        value = 0L;
+{
+    "name":"restful",
+    "main":"server.js",
+    "dependencies":{
+        "express":"latest",
+        "node-restful":"latest"
     }
-
-    public CalculatorResource(Long initialValue) {
-        this.value = initialValue;
-    }
-
-    @GET
-    public Long getValue() {
-        return value;
-    }
-
-    @Path("+{value}")
-    public CalculatorResource add(@PathParam("value") Long value) {
-        return new CalculatorResource(this.value + value);
-    }
-
-    @Path("-{value}")
-    public CalculatorResource subtract(@PathParam("value") Long value) {
-        return new CalculatorResource(this.value - value);
-    }
-
-    @Path("*{value}")
-    public CalculatorResource multiply(@PathParam("value") Long value) {
-        return new CalculatorResource(this.value * value);
-    }
-
 }
