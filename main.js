8b5e35405c275b189df664f3bb08bd6fbce080c1
Peter Leriche <R0370657@student.thomasmore.be>
2016-10-17 19:51:42 +0200

    blijkbaar moet je je dependencies opnieuw npm installen bij herstarten van netbeans / computer.
    alsook de // lijn bij package.json werd niet als commentaar gezien.aangepast bij deze

--- main.js
+++ main.js
@@ -3,18 +3,35 @@ var app = new express();
 var bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: true })); 
 
-
+app.post('/', function (req, res) {
+    res.send('typ je opdracht in de adresbalk vb: /calc/2+5');
+    
+});
+/*
 app.get("/", function (req, res) {
     // __dirname = The name of the directory that the currently executing script resides in. https://nodejs.org/docs/latest/api/globals.html#globals_dirname
-    res.sendFile( __dirname + '/index.html');
-    });
-
-app.post('/', function(request, response){
-    console.log(eval(request.body.opdracht));
-    ;
+   res.sendFile( __dirname + '/index.html');
+   });
+  
+ code overgenomen van Wibren - hij weet hiervan - om na te kijken of het bij mij werkt,
+  en als het niet werkt nagaan wat niet werkt, als het wel werkt na gaan waarom (hoe) het werkt. */
+app.post('/calc', function (req, res) {
+        
+	console.dir(req.body);  
+	var calc = req.body.bodycalc;
+	
+        var antwoord = eval(req.body.bodycalc);
+        console.dir(antwoord);
+        res.send(String(antwoord));
 });
 
-app.listen(3000);
+app.post('/calc/:opdracht',function(req, res){ 
+        var opdrachtlezen = req.params.opdracht;
+        console.log(req.params.opdracht);
+        var antwoord = eval(req.params.opdracht);
+        console.log(antwoord);
+        res.send(String(antwoord));
+});
 
 
-console.log("Hallo meneer de uil");
+app.listen(3456);
